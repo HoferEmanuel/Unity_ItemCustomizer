ec48b9590b5fc234e60bc52d182facb8dc60adf7
Emanuel Hofer <102549117+HoferEmanuel@users.noreply.github.com>
2023-07-17 10:48:05 +0200

    update

--- Scripts/ItemPartData.cs
+++ Scripts/ItemPartData.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/ItemPartData")]
 public class ItemPartData : ScriptableObject
 {
+    [Header("General")]
     public float weight;
+    public float value;
+    public List<RangedPartData> restrictedParts;
 }
